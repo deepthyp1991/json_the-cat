7397b1fa8f80ee6ee37bc80db2c2b3a405126d50
deepthyp1991 <deepthyp1991@gmail.com>
2020-12-03 02:34:42 +0000

    Working with mocha and chai

---
+++ breedFetcher.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      let breed = data[0];
+      if (breed) {
+        callback(null, data[0].description);
+      } else {
+        callback(null, 'Breed "' + breedName + '" not found');
+      }
+    }
+      
+  });
+};
+
+
+
+module.exports = fetchBreedDescription;
